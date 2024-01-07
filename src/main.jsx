b444ee17183245b3bc3fae440a1ff4e46222f128
jencler <jhuachesvasquez44@gmail.com>
2024-01-06 21:24:43 -0500

    Configuramos y creamos mi store y mi authSlice

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { LearnJsApp } from './LearnJsApp.jsx'
 import './index.css'
+import { Provider } from 'react-redux'
+import { store } from './store/store.js'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <LearnJsApp/>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <LearnJsApp />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 )
---
+++ src/store/auth/authSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState: {
+    status: 'no auth', /* auth - checking*/
+    uid: null,
+    email: null,
+    displayName: null,
+    photoURL: null,
+    errorMessage: null,
+
+  },
+  reducers: {
+    login: (state, { payload }) => {
+      state.status = payload.status
+
+    },
+    logout: (state, { payload }) => {
+
+
+    },
+    checkingCredential: (state) => {
+      state.status = 'checking'
+    }
+  }
+});
+
+
+export const { login,logout,checkingCredential } = authSlice.actions;
